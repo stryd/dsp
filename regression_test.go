0bbb2fdab00c2ff1c2e20a8d4805af2bf5d6c69a
Kun <likunarmstrong@gmail.com>
2018-01-26 15:36:39 -0700

    Adding more test cases for regression

--- regression_test.go
+++ regression_test.go
@@ -2,16 +2,36 @@ package dsp
 
 import "testing"
 
+type testCase struct {
+	points        []Point
+	wantSlope     float64
+	wantIntercept float64
+}
+
 func TestLeastSquares(t *testing.T) {
-	signal := []Point{
-		{1, 6},
-		{2, 5},
-		{3, 7},
-		{4, 10},
+	cases := []testCase{
+		testCase{
+			[]Point{
+				{1, 6},
+				{2, 5},
+				{3, 7},
+				{4, 10},
+			}, 1.4, 3.5,
+		},
+		testCase{
+			[]Point{
+				{1, 10},
+				{2, 8},
+				{3, 7},
+				{4, 5},
+			}, -1.6, 11.5,
+		},
 	}
-	slope, intercept := LeastSquares(&signal)
-	if slope != 1.4 || intercept != 3.5 {
-		t.Error("Least squares calculation is wrong")
+	for _, c := range cases {
+		slope, intercept := LeastSquares(&c.points)
+		if slope != c.wantSlope || intercept != c.wantIntercept {
+			t.Errorf("Least squares calculation is wrong, for slope, want %v, got %v; for intercept, want %v, got %v", c.wantSlope, slope, c.wantIntercept, intercept)
+		}
 	}
 
 }
